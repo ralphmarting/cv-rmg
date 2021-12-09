0ab889662aa10a0f4fa5d79820ef2e71dc402539
Martin <xy.techNOS@gmail.com>
2021-12-09 17:01:54 +0800

    Moved all animations to separate js file using jQuery.

---
+++ js/index.js
@@ -0,0 +1,46 @@
+$('.collapse-toggle').click(function() {
+  $('.collapse-toggle').toggle();
+
+  var collapseElementList = [].slice.call($('.accordion-collapse'));
+  collapseElementList.map(function(collapseEl) {
+    new bootstrap.Collapse(collapseEl);
+  });
+});
+
+$(window).scroll(function() {
+  if ($(this).scrollTop()) {
+    $('.btn-back-to-top').fadeIn(200);
+  } else {
+    $('.btn-back-to-top').fadeOut(200);
+  }
+});
+
+$('.btn-back-to-top').click(function() {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 500);
+});
+
+$('#navAbout').click(function() {
+  $('html, body').animate({
+    scrollTop: $("#about").offset().top
+  }, 500);
+});
+
+$('#navSkills').click(function() {
+  $('html, body').animate({
+    scrollTop: $("#skills").offset().top
+  }, 500);
+});
+
+$('#navCareer').click(function() {
+  $('html, body').animate({
+    scrollTop: $("#career").offset().top
+  }, 500);
+});
+
+$('#navEducation').click(function() {
+  $('html, body').animate({
+    scrollTop: $("#education").offset().top
+  }, 500);
+});
