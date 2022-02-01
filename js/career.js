3583125d7bd8591c40ccff76bfbc022ba3550575
Martin <xy.techNOS@gmail.com>
2022-02-01 20:30:01 +0800

    Test populating using JS module

---
+++ js/career.js
@@ -0,0 +1,14 @@
+career = [{
+    key: 1,
+    startDate: Date.UTC(2015, 07),
+    endDate: Date.UTC(2016, 02),
+    role: "Intern",
+    company: "International Business Machines (IBM)",
+    description: `A series of training sessions that furthered my knowledge in
+      Web Development, Java, J2EE, MVC frameworks, Spring, Struts, JSF, JSP,
+      SQLite, MySQL. Even testing methodologies.`,
+    technologies: `Web Development, Eclipse, Java, J2EE, MVC, Database,
+      Testing Concepts`,
+    location: "Cebu City, Cebu, Philippines"
+  }
+]
